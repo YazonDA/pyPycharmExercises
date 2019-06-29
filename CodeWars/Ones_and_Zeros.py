076c2150fabef04bd89d3fd394223af15c6e6a99
YazonDA <yazonda.29@gmail.com>
2019-06-29 17:11:53 +0300

    More one TEST

--- CodeWars/Ones_and_Zeros.py
+++ CodeWars/Ones_and_Zeros.py
@@ -10,3 +10,28 @@ a = binary_array_to_number(mass)
 print('answer = ', a)
 print(pow(2, 10))
 print(sum(mass, 2))
+
+
+# I want to understand the following four solution
+#
+# !!! This is not understood enough !!!
+# def binary_array_to_number(arr):
+#     append_bit = lambda n, b: n << 1 | b
+#     return reduce(append_bit, arr)
+#
+# !!! This is not understood enough !!!
+# def binary_array_to_number(arr):
+#     return sum(digit * 2**i for i, digit in enumerate(reversed(arr)))
+#
+# !!! This is not understood enough !!!
+# def binary_array_to_number(arr):
+#     if type(arr) is list and len(arr)>0:
+#       return int(''.join(str(e) for e in arr), 2)
+#     return -1
+#
+# !!! This is not understood enough !!!
+# def binary_array_to_number(arr, acc = 0):
+#     if len(arr) == 0:
+#         return acc
+#     acc = (acc << 1) | arr.pop(0)
+#     return binary_array_to_number(arr, acc)
--- test.py
+++ test.py
@@ -1,21 +1,11 @@
-""" Calculate the cost of a liter in the store packing box
+""" Testing map - function
 
 """
-size_arr = [0, 0, 0, 0]
-name_arr = ['size_1', 'size_2', 'size_3', 'price']
-i = -1
 
-while True:
-    i = (i + 1) % 4
-    s = input(name_arr[i] + ' = ')
-    if s != '//':
-        if i == 3:
-            size_arr[i] = float(s)
-            vol = 1
-            for j in range(3):
-                vol = vol * size_arr[j]
-            print(f'{size_arr[0]}x{size_arr[1]}x{size_arr[2]} -- {vol:.2f}m3 & {size_arr[3] / vol:.2f}p/m3')
-        else:
-            size_arr[i] = int(s) / 1000
-    else:
-        break
+
+def f(x):
+    return x ** 2
+
+
+a = [0, 1, 2, 3]
+print(list(map(f, a)))
