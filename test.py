58252afa1e4d7bb30bbdf6ff5e33883a97dc6a13
YazonDA <yazonda.29@gmail.com>
2019-06-30 00:13:33 +0300

    next task in CodeWar - Square(n) Sum in test ver.1

--- test.py
+++ test.py
@@ -11,3 +11,11 @@ a = [0, 3, 4, 5]
 
 print(square_sum(a))
 print(a)
+
+
+# --- This is not understood enough ---
+# +++
+# this algorithm is more simple
+# +++
+# def square_sum(numbers):
+#     return sum(x ** 2 for x in numbers)
