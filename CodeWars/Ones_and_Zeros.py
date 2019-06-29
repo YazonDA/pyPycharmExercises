c7822a6f9faa725450b8d69f33cdb22e02836c96
YazonDA <yazonda.29@gmail.com>
2019-06-29 18:57:40 +0300

    and More one test for more one algorithm for more one Ones_and_Zero

--- CodeWars/Ones_and_Zeros.py
+++ CodeWars/Ones_and_Zeros.py
@@ -29,7 +29,10 @@ print(sum(mass, 2))
 # def binary_array_to_number(arr):
 #     return sum(digit * 2**i for i, digit in enumerate(reversed(arr)))
 #
-# !!! This is not understood enough !!!
+# --- This is not understood enough ---
+# +++
+# this algorithm is more efficient then mine
+# +++
 # def binary_array_to_number(arr):
 #     if type(arr) is list and len(arr)>0:
 #       return int(''.join(str(e) for e in arr), 2)
--- test.py
+++ test.py
@@ -4,9 +4,9 @@ with another algorithm
 
 
 def binary_array_to_number(arr):
-    s = sum(d * 2 ** i for i, d in enumerate(reversed(arr)))
+    s = int(''.join(str(e) for e in arr), 2)
     return s
 
 
-mass = [1, 1, 0, 1]
+mass = [1, 0, 0, 1]
 print('new answer = ', binary_array_to_number(mass))
