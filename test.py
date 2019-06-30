0a198c501913ed050ba4e24a5006b5256b66d75e
YazonDA <yazonda.29@gmail.com>
2019-07-01 01:27:25 +0300

    next task in CodeWar - Vasya-Clerk - in test

--- test.py
+++ test.py
@@ -1,41 +1,64 @@
-""" Testing for Who likes it?
-Tags - FUNDAMENTALS FORMATTING ALGORITHMS STRINGS
+""" Testing for Vasya - Clerk
+Tags - FUNDAMENTALS MATHEMATICS ALGORITHMS NUMBERS GAMES
 """
 
-def likes(names):
-    count_more = str(len(names) - 2)
-    ln = len(names) if len(names) <= 4 else 4
-    names.extend([''] * (4 - ln))
-    end_answer_1 = 'likes this'
-    end_answer_2 = 'like this'
-    my_dict = {0: ['no one', end_answer_1],
-               1: [names[0], end_answer_1],
-               2: [names[0] + ' and ' + names[1], end_answer_2],
-               3: [names[0] + ', ' + names[1] + ' and ' + names[2], end_answer_2],
-               4: [names[0] + ', ' + names[1] + ' and ' + count_more + ' others', end_answer_2]}
-    return F'{my_dict[ln][0]} {my_dict[ln][1]}'
+
+def tickets(people):
+    cash_box = {25: 0,
+                50: 0,
+                100: 0}
+    # This string for service print only
+    # cash_sum = lambda s: sum(s[x] * x for x in s)
+    for pay in people:
+        change = pay - 25
+        cash_box[pay] += 1
+        if change == 25:
+            cash_box[25] -= 1
+        elif change == 75:
+            cash_box[25] -= 1
+            if cash_box[50] == 0:
+                cash_box[25] -= 2
+            else:
+                cash_box[50] -= 1
+        if cash_box[25] < 0 or cash_box[50] < 0:
+            return "NO"
+    return "YES"
 
 
-# a = []
-# a = ['Peter']
-# a = ['Jacob', 'Alex']
-# a = ['Max', 'John', 'Mark']
-# a = ['Alex', 'Jacob', 'Mark', 'Max']
-a = ['Alex', 'Jacob', 'Mark', 'Max', 'Peter']
+# a = [25, 25, 50]
+# a = [25, 100]
+# a = [25, 25, 50, 100, 25, 100]
+# a = [25, 25, 50, 50, 100, 25, 25, 25, 100]
+a = [25, 25, 50, 25, 25, 100, 25, 25, 25, 100]
 
-print(a)
-print(likes(a))
+# print(a)
+print(tickets(a))
 
-# this solution is CORRECT
+# this solutions is CORRECT
 #   +++
-# def likes(names):
-#     n = len(names)
-#     return {
-#         0: 'no one likes this',
-#         1: '{} likes this',
-#         2: '{} and {} like this',
-#         3: '{}, {} and {} like this',
-#         4: '{}, {} and {others} others like this'
-#         }[min(4, n)].format(*names[:3], others=n-2)
+# def tickets(people):
+#   till = {100.0:0, 50.0:0, 25.0:0}
+#   for paid in people:
+#     till[paid] += 1
+#     change = paid-25.0
+#     for bill in (50,25):
+#       while (bill <= change and till[bill] > 0):
+#         till[bill] -= 1
+#         change -= bill
+#     if change != 0:
+#       return 'NO'
+#   return 'YES'
+#   +++
+# def tickets(a):
+#     n25 = n50 = n100 = 0
+#     for e in a:
+#         if   e==25            : n25+=1
+#         elif e==50            : n25-=1; n50+=1
+#         elif e==100 and n50>0 : n25-=1; n50-=1
+#         elif e==100 and n50==0: n25-=3
+#         if n25<0 or n50<0:
+#             return 'NO'
+#     return 'YES'
+#   +++
 #   ---
-#   my solution difficult :((((
+#   my solution NOT OPTIMAL :((((
