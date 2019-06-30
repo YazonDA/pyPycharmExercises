e709e87815ab204011f2966edbb57c8d50a7833b
YazonDA <yazonda.29@gmail.com>
2019-06-30 16:59:13 +0300

    next task in CodeWar - Beginner Series #3 Sum of Numbers - in test

--- test.py
+++ test.py
@@ -1,13 +1,23 @@
-""" Testing for Reversed Strings
-
+""" Testing for Beginner Series #3 Sum of Numbers
+try Built-in Types - Range
 """
 
 
-def solution(string):
-    return string[::-1]
+# def get_sum(a, b):
+#     s = 0
+#     if a > b:
+#         a, b = b, a
+#     for i in range(a, b + 1):
+#         s += i
+#     return s
 
 
-a = ' l ;'
+def get_sum(a, b):
+    if a > b: a, b = b, a
+    return sum(i for i in range(a, b + 1))
 
-print(solution(a))
-print(a)
+
+x = 2
+y = 1
+
+print(get_sum(x, y))
