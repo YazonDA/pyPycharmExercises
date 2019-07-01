1f3c9b761f82331ddabf5e219ebbc3665d130bd2
YazonDA <yazonda.29@gmail.com>
2019-07-01 03:24:03 +0300

    next task in CodeWar - Sort the odd - in test

--- test.py
+++ test.py
@@ -1,64 +1,29 @@
-""" Testing for Vasya - Clerk
-Tags - FUNDAMENTALS MATHEMATICS ALGORITHMS NUMBERS GAMES
+""" Testing for Sort the odd
+Tags - FUNDAMENTALS ARRAYS
 """
 
 
-def tickets(people):
-    cash_box = {25: 0,
-                50: 0,
-                100: 0}
-    # This string for service print only
-    # cash_sum = lambda s: sum(s[x] * x for x in s)
-    for pay in people:
-        change = pay - 25
-        cash_box[pay] += 1
-        if change == 25:
-            cash_box[25] -= 1
-        elif change == 75:
-            cash_box[25] -= 1
-            if cash_box[50] == 0:
-                cash_box[25] -= 2
-            else:
-                cash_box[50] -= 1
-        if cash_box[25] < 0 or cash_box[50] < 0:
-            return "NO"
-    return "YES"
+def sort_array(source_array):
+    odd_arr = sorted(x for x in source_array if x % 2)
+    j = 0
+    for i in range(len(source_array)):
+        if source_array[i] % 2:
+            source_array[i] = odd_arr[j]
+            j += 1
+    return source_array
 
 
-# a = [25, 25, 50]
-# a = [25, 100]
-# a = [25, 25, 50, 100, 25, 100]
-# a = [25, 25, 50, 50, 100, 25, 25, 25, 100]
-a = [25, 25, 50, 25, 25, 100, 25, 25, 25, 100]
+a = [5, 3, 2, 8, 1, 4]      #[1, 3, 2, 8, 5, 4]
+# a = [5, 3, 1, 8, 0]         #[1, 3, 5, 8, 0]
 
 # print(a)
-print(tickets(a))
+print(sort_array(a))
 
-# this solutions is CORRECT
+# this solutions is VERY GOOD (imho)
 #   +++
-# def tickets(people):
-#   till = {100.0:0, 50.0:0, 25.0:0}
-#   for paid in people:
-#     till[paid] += 1
-#     change = paid-25.0
-#     for bill in (50,25):
-#       while (bill <= change and till[bill] > 0):
-#         till[bill] -= 1
-#         change -= bill
-#     if change != 0:
-#       return 'NO'
-#   return 'YES'
-#   +++
-# def tickets(a):
-#     n25 = n50 = n100 = 0
-#     for e in a:
-#         if   e==25            : n25+=1
-#         elif e==50            : n25-=1; n50+=1
-#         elif e==100 and n50>0 : n25-=1; n50-=1
-#         elif e==100 and n50==0: n25-=3
-#         if n25<0 or n50<0:
-#             return 'NO'
-#     return 'YES'
+# def sort_array(arr):
+#   odds = sorted((x for x in arr if x%2 != 0), reverse=True)
+#   return [x if x%2==0 else odds.pop() for x in arr]
 #   +++
 #   ---
 #   my solution NOT OPTIMAL :((((
