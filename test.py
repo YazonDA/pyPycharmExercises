4522cbfc151cd7a231c126fa23a50b8f14a3b485
YazonDA <yazonda.29@gmail.com>
2019-07-14 04:14:02 +0300

    next task in CodeWar -The Millionth Fibonacci Kata- I did it, but it is need Python2

--- test.py
+++ test.py
@@ -1,20 +1,34 @@
-""" Testing for - Numbers that are a power of their sum of digits
+""" Testing for - The Millionth Fibonacci Kata
 
 find solutions
 """
 
 
-def power_sumDigTerm(n):
-    s = []
-    s.extend(i ** p
-             for p in range(2, 30)
-             for i in range(7, 99)
-             if sum(map(int, str(i ** p))) == i)
-    s.sort()
-    return s[n - 1]
+def fib(n):
+    def power(m1, n, m2, mult_m):
+        if n == 0:
+            ans = m2
+        elif n == 1:
+            ans = m1
+        else:
+            y = power(m1, n // 2, m2, mult_m)
+            y = mult_m(y, y)
+            if n % 2:
+                y = mult_m(m1, y)
+            ans = y
+        return ans
 
+    def make_matr(n):
+        return [[1 if i == j else 0 for i in range(n)]
+                for j in range(n)]
 
-a = 15
-answer = power_sumDigTerm(a)
-print(answer)
+    def mult_matr(a, b):
+        return [[sum(i * j for i, j in zip(Ya, Xb))
+                for Xb in list(zip(*b))] for Ya in a]
+
+    return power([[1, 1], [1, 0]], n, make_matr(2), mult_matr)[0][1]
+
+a = 14
+for i in range(a):
+    print(fib(i))
 # print(len(answer))
