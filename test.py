2be3d908bb35cad066c214e06e0f7fd691183da4
YazonDA <yazonda.29@gmail.com>
2019-07-22 18:08:10 +0300

    next CodeWarS Task -Human readable duration format- complited but not good

--- test.py
+++ test.py
@@ -3,10 +3,22 @@
 find solutions
 """
 
-import time
 
+seconds = 36620012
+ans_dict = [60, 60, 24, 365, 365]
+ans_dict_1 = [1, 60, 60*60, 24*3600, 365*24*3600, 1]
+ans_list = [(seconds // (ans_dict_1[i])) % ans_dict[i] for i in range(5)]
 
-print(time.ctime(0))
+print(ans_list)
+ans_list = []
 
-print(time.strftime('%Y - %m - %d - %H:%M:%S', time.gmtime(0)))
+for i in range(5):
+    a3 = ans_dict[i]
+    a2 = ans_dict_1[i]
+    a4 = seconds // a2
+    a1 = a4 % a3
+    ans_list.append(a1)
+    # seconds = seconds // ans_dict[i]
 
+print(ans_list)
+print(seconds)
