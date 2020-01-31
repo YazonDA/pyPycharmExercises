06a8ad539857ee99f73033e0c4c3298f8c5b9933
YazonDA <yazonda.29@gmail.com>
2020-01-31 09:46:58 +0300

    first try to create the bot

--- FzzBzz.py
+++ FzzBzz.py
@@ -1,8 +1,9 @@
 def fb(lst):
 	for i in range(len(lst)):
+		'''	
 		n = lst[i]
 		lst[i] = "Fizz"*(n%3==0) + "Buzz"*(n%5==0) or n
-'''
+		'''
 		if lst[i]%3 == 0 and lst[i] != 0:
 			if lst[i]%5 == 0:
 				lst[i] = 'FizzBuzz'
@@ -10,7 +11,7 @@ def fb(lst):
 				lst[i] = 'Fizz'
 		elif lst[i]%5 == 0 and lst[i] != 0:
 			lst[i] = 'Buzz'
-'''
+	
 a = list(range(1, 51))
 print(a)
 fb(a)
